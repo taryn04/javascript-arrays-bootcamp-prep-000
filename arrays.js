5ac547c61292b35762dc261cf9105b00fa035be7
Taryn Boni <taryn04@live.com>
2018-04-03 19:01:00 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -5,7 +5,7 @@ var chocolateBars = [
   'skittles'
   ]
   
-function addElementToBeginningOfArray([array], element){
+function addElementToBeginningOfArray(array, element){
   return array.unshift(element)
 }
 
