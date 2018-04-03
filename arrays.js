6128607d44f6641ff4c0d54d90939c819774b0fb
Taryn Boni <taryn04@live.com>
2018-04-03 19:00:16 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -6,7 +6,7 @@ var chocolateBars = [
   ]
   
 function addElementToBeginningOfArray([array], element){
-  [array].unshift(element)
+  return array.unshift(element)
 }
 
 function destructivelyAddElementToBeginningOfArray(){
