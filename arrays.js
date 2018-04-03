d887e303eeed1be187dab893abba72561c56faa1
Taryn Boni <taryn04@live.com>
2018-04-03 19:21:02 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -38,4 +38,11 @@ function removeElementFromBeginningOfArray(array){
   return array.slice(1)
 }
 
-function 
+function destructivelyRemoveElementFromEndOfArray(array){
+  array.pop()
+  return array
+}
+
+function removeElementFromEndOfArray(array){
+  return array.slice(0, array.length - 1)
+}
