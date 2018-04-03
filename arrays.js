396f1e42345236c1656732a3fe99ae070dd5057d
Taryn Boni <taryn04@live.com>
2018-04-03 19:16:08 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -28,3 +28,7 @@ function destructivelyAddElementToEndOfArray(array,element){
 function accessElementInArray(array, index){
   return array[index]
 }
+
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  return array.shift()
+}
